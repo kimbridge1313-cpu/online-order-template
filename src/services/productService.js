0e0de95a10b8af5109e2a2206b6ed4a20339f6ca
Lowen <lowenhsu.tw@gmail.com>
2026-07-13 16:30:19 +0800

    Switch product service by environment

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,2 +1,5 @@
+import { env } from '../config/env'
 import { mockProductService } from './mockProductService'
-export const productService = mockProductService
+import { firebaseProductService } from './firebaseProductService'
+
+export const productService = env.useMockData ? mockProductService : firebaseProductService
